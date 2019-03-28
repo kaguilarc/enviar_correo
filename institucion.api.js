5423927a3b7604977d601d6ce51f67c19426b841
kaguilarc <47117619+kaguilarc@users.noreply.github.com>
2019-03-27 18:10:33 -0600

    Update institucion.api.js

--- institucion.api.js
+++ institucion.api.js
@@ -1,6 +1,6 @@
 'use strict';
 const peticion_registro = require('../peticiones_registro/peticiones_registro.model');
-const nodeMailer = require("nodemailer");
+const nodeMailer = require("nodemailer");//instalar esta dependencia
 const institucionModel = require('./instituciones.model');
 
 /**
@@ -10,9 +10,11 @@ const transporter = nodeMailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'cenfochocolate@gmail.com',
-        pass: 'grupochocolate1'
+        pass: 'xxxxxxxxxxxx'
     }
 });
+
+
 
 
 module.exports.registrar = function (req, res) {
@@ -59,17 +61,19 @@ module.exports.registrar = function (req, res) {
             if (institucion) {
                 res.json({
                     success: false,
-                    msj: 'La cédula ' + req.body.cedula + ' ya se encuentra registrada'
+                    msj: 'La cÃ©dula ' + req.body.cedula + ' ya se encuentra registrada'
                 });
                 /**
                  * Sino, entonces quiere decir que la insticion no se encuentra registrada
                  * ...procede a guardarla
-                 * dentro de esa función tambien se devuelven json, en caso de exito o error
+                 * dentro de esa funciÃ³n tambien se devuelven json, en caso de exito o error
                  */
             } else {
+                
                 /**
                  * generamos un codigo de manera aleatoria
                  * */
+                
                 const codigo_registro = Math.trunc(Math.random() * (0, 999999));
 
                 /**
@@ -90,6 +94,8 @@ module.exports.registrar = function (req, res) {
                          * 
                          * 1. creamos el modelo
                          * */
+                        
+                        
                         let nuevaPeticion = new peticion_registro({
                             cedula_compania: req.body.cedula,
                             codigo: codigo_registro
@@ -110,10 +116,11 @@ module.exports.registrar = function (req, res) {
                                 /**
                                  * El modelo se guardo, procedemos a enviar el correo
                                  * */
+                                
                                 let mailOptions = {
                                     from: 'cenfochocolate@gmail.com',
                                     to: nuevaInstitucion.correo,
-                                    subject: 'confirmación de Registro',
+                                    subject: 'confirmaciÃ³n de Registro',
                                     html: `<html>
                            <span>el codigo de confirmacion es el ${codigo_registro}</span>
                            </html>`
@@ -122,10 +129,10 @@ module.exports.registrar = function (req, res) {
 
                                 transporter.sendMail(mailOptions, function (error, info) {
                                     if (error) {
-                                        console.log('dsdsdfdsfAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+                                        //console.log('dsdsdfdsfAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
                                         console.log(error);
                                     } else {
-                                        console.log('correo enviado correctamente' + nuevaInstitucion.correo);
+                                       // console.log('correo enviado correctamente' + nuevaInstitucion.correo);
                                     }
                                 });
 
@@ -179,3 +186,20 @@ module.exports.listar_instituciones = function (req, res) {
         }
     )
 };
+
+
+
+'use strict';
+
+let mongoose = require('mongoose');
+
+let peticion_registroSchema = new mongoose.Schema({
+    cedula_compania: { type: String, requerid: true },
+    codigo: { type: String, required: true },
+    fecha_peticion: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Peticion Registro', peticion_registroSchema);
+
+
+
